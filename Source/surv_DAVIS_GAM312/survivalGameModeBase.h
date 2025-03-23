a854ab2ef4d5c36e2a9356493792d76f216caffc
Ross Davis <Ross.Davis@snhu.edu>
2025-03-23 13:56:18 -0700

    Created player, inputs, gamemodebase

---
+++ Source/surv_DAVIS_GAM312/survivalGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "survivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURV_DAVIS_GAM312_API AsurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
